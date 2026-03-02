f7c2154593b3db5cdb23358002f405efcee301f2
mandys-k <amanda.khalil@iths.se>
2026-03-02 23:45:58 +0100

    börjat timer funktionen

---
+++ my-project/src/components/CountdownTimer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="timer-container">
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+
+        <button type="button" class="play-button">
+            <span class="material-symbols-outlined">
+                timer_play
+            </span>
+        </button>
+
+        <button type="button" class="timer-button">
+            <span class="material-symbols-outlined">
+                play_circle
+            </span>
+        </button>
+    </div>
+
+    <!-- <span class="material-symbols-outlined">
+        replay
+    </span> -->
+</template>
