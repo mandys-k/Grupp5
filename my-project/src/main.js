32a54eff3598f622bd17b1a506ee30c07cc4840a
Scott <scottabraham87@gmail.com>
2026-03-02 18:36:06 +0200

    Add import for bootstrap

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
+// Import Bootstrap and BootstrapVue CSS files - SAK
+import { createBootstrap } from 'bootstrap-vue-next' 
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 createApp(App).mount("#app");
