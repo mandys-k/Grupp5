5db97f5c4ab1bc97c102696ccf68999474bc84fc
Scott <scottabraham87@gmail.com>
2026-03-04 18:26:57 +0200

    /File deletions and files added.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+
+
+
+<template>
+    <Hero msg="Your next adventure begins with the next word."/>
+    <GroupInfoCard />
+    <CustomerReview />
+</template>
+
+<script>
+  import CustomerReview from "../components/CustomerReview.vue";
+  import Hero from "../components/Hero.vue";
+  import GroupInfoCard from "../components/GroupInfoCard.vue";
+
+  export default {
+    components: {
+      CustomerReview,
+      Hero,
+      GroupInfoCard,
+
+    }
+  };
+</script>
+
