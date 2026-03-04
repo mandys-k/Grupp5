9a23946e9d5b91e7847b69260c13566ac424b48f
Erika Matsumoto <matmoterika816@gmail.com>
2026-03-04 15:47:07 +0100

    Add CustomerReview.vue and changed HomeView

---
+++ my-project/src/components/CustomerReview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="review-container d-flex align-items-center justify-content-center text-white text-left">
+    <b-container class="bv-example-row">
+  <b-row>
+    <b-col sm="4"><b-img rounded src="/assets/CustomerReview.png" fluid alt="Responsive image"></b-img></b-col>
+    <b-col sm="8">
+      <h1>Sophia Martinez</h1>
+      <h1>Business Consultant</h1>
+      <p>“I travel frequently for work, and this app helps me prepare for meetings and networking abroad. The business-focused vocabulary is exactly what I needed. It saves me time and boosts my confidence instantly.”</p>
+    </b-col>
+  </b-row>
+
+</b-container>
+  </div>
+</template>
+
+
+<style scoped>
+.review-container {
+  background-image:
+    linear-gradient(
+      rgba(255, 239, 70, 0.85),
+      rgba(7, 213, 241, 0.85)
+    );
+  min-height: 20vh;
+  padding-top: 80px;   
+  padding-bottom: 80px;
+  margin-bottom: 40px;
+}
+</style>
