28a59b94e1feb73b5df7e4aae39b4d1963baefad
mandys-k <amanda.khalil@iths.se>
2026-03-05 17:05:36 +0100

    created a new file themeStore.js and uploaded new images

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -1,6 +1,18 @@
+<!-- <script>
+import { mapStores } from "pinia"
+import { useThemeStore } from "../dataStorage/themeStore.js"
+
+export default {
+    computed: {
+        ...mapStores(useThemeStore)
+    }
+}
+</script>
+
 <template>
-    <button type="button">
+    <button @click="themeStore.onDarkMode" type="button">
         <span class="material-symbols-outlined"> circle </span>
         Dark mode
     </button>
 </template>
+ -->
