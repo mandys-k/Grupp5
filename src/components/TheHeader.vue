77623aab7808140930f550ae0de7b83e4b755764
Erika Matsumoto <matmoterika816@gmail.com>
2026-03-06 23:35:19 +0100

    Edited The header and added Hambueger menu

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,29 +1,64 @@
 <template>
-  <div class="header-navbar d-flex align-items-left justify-content-left text-white text-left">
-    <b-container class="bv-example-row px=0" fluid >
-    <b-row class="align-items-center">
-        <b-col sm="6" class="triplingo">
+  <header class="header-navbar text-white">
+    <b-container fluid class="px-4">
+      <b-row class="align-items-center">
+
+
+        <b-col cols="6" md="4" class="triplingo">
           <RouterLink to="/"><h2>TripLingo</h2></RouterLink>
         </b-col>
-        <b-col sm="6" class="d-flex justify-content-end nav-links">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/levels">Levelmap</RouterLink>
-            <RouterLink to="/notepad">Notepad</RouterLink>
-            <RouterLink to="/saved">Saved</RouterLink>
-            <RouterLink to="/quiz">Quiz</RouterLink>
-            <RouterLink to="/login">User</RouterLink>
+
+
+        <b-col cols="6" class="d-md-none text-end">
+          <button class="hamburger" @click="toggleMenu">
+            ☰
+          </button>
         </b-col>
-    </b-row>
+
+        <b-col md="8" class="d-none d-md-flex justify-content-end nav-links">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/levelmap">Levelmap</RouterLink>
+          <RouterLink to="/notepad">Notepad</RouterLink>
+          <RouterLink to="/saved">Saved</RouterLink>
+          <RouterLink to="/quiz">Quiz</RouterLink>
+          <RouterLink to="/user">User</RouterLink>
+        </b-col>
+
+      </b-row>
     </b-container>
-    
-  </div>
+
+
+    <div v-if="hamburgerClicked" class="mobile-menu d-md-none">
+      <RouterLink to="/" @click="toggleMenu">Home</RouterLink>
+      <RouterLink to="/levelmap" @click="toggleMenu">Levelmap</RouterLink>
+      <RouterLink to="/notepad" @click="toggleMenu">Notepad</RouterLink>
+      <RouterLink to="/saved" @click="toggleMenu">Saved</RouterLink>
+      <RouterLink to="/quiz" @click="toggleMenu">Quiz</RouterLink>
+      <RouterLink to="/user" @click="toggleMenu">User</RouterLink>
+    </div>
+
+  </header>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      hamburgerClicked:false
+    }
+  },
+  methods:{
+    toggleMenu(){
+      this.hamburgerClicked = !this.hamburgerClicked
+    }
+  }
+}
+</script>
 
 <style>
-.header-navbar {
-    background-color:rgba(11, 11, 98, 0.85);
-    color: white;
+
+.header-navbar{
+  background-color: rgba(11, 11, 98, 0.9);
 }
 
 .triplingo a{
@@ -31,17 +66,45 @@
   text-decoration: none;
 }
 
-.nav-links {
-  display: flex;
-  gap: 30px;   
+.nav-links{
+  display:flex;
+  gap:30px;
 }
 
-.nav-links a {
-  color: white;
-  text-decoration: none;
+.nav-links a{
+  color:white;
+  text-decoration:none;
 }
 
-.nav-links a:hover {
-  text-decoration: underline;
+.nav-links a:hover{
+  text-decoration:underline;
 }
+
+
+.hamburger{
+  background:none;
+  border:none;
+  color:white;
+
+}
+
+
+.mobile-menu{
+  display:flex;
+  flex-direction:column;
+  gap:15px;
+  padding:15px;
+  background-color:rgba(11,11,98,0.95);
+}
+
+.mobile-menu a{
+  color:white;
+  text-decoration:none;
+ 
+}
+
+.mobile-menu a:hover{
+  text-decoration:underline;
+}
+
 </style>
