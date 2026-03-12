3e2040d02b53fc19a504773b4d565a65705221fa
Scott <scottabraham87@gmail.com>
2026-03-12 10:59:33 +0100

    add base URL to vite.config so that github pages URL works

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 export default defineConfig({
+  base: '/Grupp5/',
   plugins: [
     vue(),
     Components({
