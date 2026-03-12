afa46eb164a59fac4b62f63399f5ff4d26555c4b
Scott <scottabraham87@gmail.com>
2026-03-12 12:13:38 +0100

    changed alias details for images

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   base: '/Grupp5/',
@@ -10,5 +11,10 @@ export default defineConfig({
     Components({
       resolvers: [BootstrapVueNextResolver()]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
