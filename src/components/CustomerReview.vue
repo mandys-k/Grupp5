caafa479ec31cd7e3ee33955d9c7397fc3cab633
Scott <scottabraham87@gmail.com>
2026-03-12 11:53:23 +0100

    Changed assets path in 8 files removing the first / to fix images not appearing in GH pages

--- src/components/CustomerReview.vue
+++ src/components/CustomerReview.vue
@@ -2,7 +2,7 @@
   <div class="review-container d-flex align-items-center justify-content-center text-white text-left">
     <b-container class="bv-example-row">
   <b-row>
-    <b-col sm="4"><b-img rounded src="/assets/CustomerReview.png" fluid alt="Responsive image"></b-img></b-col>
+    <b-col sm="4"><b-img rounded src="assets/CustomerReview.png" fluid alt="Responsive image"></b-img></b-col>
     <b-col sm="8">
       <h1><b>Sophia Martinez</b></h1>
       <h1>Business Consultant</h1>
