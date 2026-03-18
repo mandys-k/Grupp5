869cfe076a20a712a3dd9184d605c44ad95b5771
Scott <scottabraham87@gmail.com>
2026-03-18 13:12:45 +0100

    Updated eslint config file so that cypress tests are not flagged as problems

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,29 @@ import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  // General JS / Vue files
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  // Vue essential rules
   pluginVue.configs["flat/essential"],
+
+  // Cypress tests
+  {
+    files: ["cypress/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        cy: "readonly",
+        Cypress: "readonly",
+      },
+    },
+  },
 ]);
