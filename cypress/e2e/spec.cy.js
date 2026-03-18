79dd56163855a7e4854a19f40dc93b5bfa26c508
Scott <scottabraham87@gmail.com>
2026-03-18 12:59:03 +0100

    add cypress test for accessibility tetsing using cypress-axe plugin, all team memebers need to run npm install

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,4 +8,11 @@ describe('learn languages website', () => {
     cy.get(".min-sec-container").should("not.contain", "00:00")
     cy.contains("Time's up!").should("not.exist")
   })
+}),
+
+// Accessibility test - uses cyporess-axe npm to check for accessibility issues on the page
+it("has no accessibility issues", () => {
+    cy.visit("/")
+    cy.injectAxe()
+    cy.checkA11y()
 })
