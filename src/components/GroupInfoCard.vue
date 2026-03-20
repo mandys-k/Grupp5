f8d3c1c82f916f86aa40d79b1d4f0ec77e643b0b
Scott <scottabraham87@gmail.com>
2026-03-20 11:31:45 +0100

    fix sematic issue caused by bootstrap and button contrast problem

--- src/components/GroupInfoCard.vue
+++ src/components/GroupInfoCard.vue
@@ -6,7 +6,7 @@
     <div class="group-card">
         <img src="@/assets/InterestTravelers.png" alt="Leisure Travelers" style="width:100%">
         <div class="container">
-            <h4><b>Leisure Travelers</b></h4> 
+            <h2><b>Leisure Travelers</b></h2> 
             <p>Learn essential phrases to feel confident, connect with locals, and navigate new places with ease.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -17,7 +17,7 @@
     <div class="group-card">
         <img src="@/assets/BusinessTravelers.png" alt="Business Travelers" style="width:100%">
         <div class="container">
-            <h4><b>Business Travelers</b></h4> 
+            <h2><b>Business Travelers</b></h2> 
             <p>Build practical language skills for meetings, networking, and frequent international travel.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -28,7 +28,7 @@
     <div class="group-card">
         <img src="@/assets/DigitalNomads.png" alt="Digital Nomads" style="width:100%">
         <div class="container">
-            <h4><b>Digital Nomads</b></h4> 
+            <h2><b>Digital Nomads</b></h2> 
             <p>Develop deeper language skills to integrate, work remotely, and thrive abroad long-term.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -40,7 +40,7 @@
     <div class="group-card">
         <img src="@/assets/LanguageLearners.png" alt="Language Enthusiasts" style="width:100%">
         <div class="container">
-            <h4><b>Language Enthusiasts</b></h4> 
+            <h2><b>Language Enthusiasts</b></h2> 
             <p>Learn a new language for personal growth, curiosity, and the joy of communication.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -51,7 +51,7 @@
     <div class="group-card">
         <img src="@/assets/TravelingCompanies.png" alt="Traveling Companies" style="width:100%">
         <div class="container">
-            <h4><b>Traveling Companies</b></h4> 
+            <h2><b>Traveling Companies</b></h2> 
             <p> Support employees with structured language training for smoother international operations.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -62,7 +62,7 @@
     <div class="group-card">
         <img src="@/assets/EducationalInstitutions.png" alt="Educational Institutions" style="width:100%">
         <div class="container">
-            <h4><b>Educational Institutions</b></h4> 
+            <h2><b>Educational Institutions</b></h2> 
             <p>Offer travelers language support to improve confidence and customer experience abroad.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -73,7 +73,7 @@
     <div class="group-card">
         <img src="@/assets/TourismAgencies.png" alt="Tourism Agencies" style="width:100%">
         <div class="container">
-            <h4><b>Tourism Agencies</b></h4> 
+            <h2><b>Tourism Agencies</b></h2> 
             <p>Offer travelers language support to improve confidence and customer experience abroad.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -84,7 +84,7 @@
     <div class="group-card">
         <img src="@/assets/Financiers.png" alt="Financiers" style="width:100%">
         <div class="container">
-            <h4><b>Financiers</b></h4> 
+            <h2><b>Financiers</b></h2> 
             <p>Invest in scalable language solutions designed for a global, mobile audience.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -95,7 +95,7 @@
     <div class="group-card">
         <img src="@/assets/MarketingDepartment.png" alt="Marketing Department" style="width:100%">
         <div class="container">
-            <h4><b>Marketing Department</b></h4> 
+            <h2><b>Marketing Department</b></h2> 
             <p>Leverage insights and user engagement tools to reach international markets effectively.</p> 
             <router-link to="/levels">
               <button class="groupinfo-btn">Start Learning</button>
@@ -113,10 +113,10 @@
 
 .group-card-container{
   display: flex;
-  flex-wrap: wrap;          
+  flex-wrap: wrap;
   justify-content: center;
-  gap: 27px;  
-  margin-top: 20px;
+  gap: 27px;
+  margin-top: 40px;
   margin-bottom: 100px;              
 }
 
@@ -131,8 +131,8 @@
 }
 
 .groupinfo-btn{
-  background-color: rgb(255, 123, 0);
-  color:aliceblue;
+  background-color: #C05500;
+  color: white;
   padding: 10px 18px;
   border: none;
   border-radius: 8px;
@@ -140,7 +140,7 @@
 }
 
 .groupinfo-btn:hover{
-  background-color: orange;
+  background-color: #A84A00;
 }
 
 @media (max-width: 900px) {
