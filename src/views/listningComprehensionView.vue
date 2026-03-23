4c1d92834bb0d158526db671f06c8d6ef3e1786c
Erika Matsumoto <matmoterika816@gmail.com>
2026-03-23 16:07:11 +0100

    Fixed timers collopse

--- src/views/listningComprehensionView.vue
+++ src/views/listningComprehensionView.vue
@@ -1,17 +1,22 @@
-<script>
-    import CountdownTimer from "../components/CountdownTimer.vue"
-    import listningComprehension from "../components/listningComprehension.vue"
+<script setup>
+import { ref } from "vue"
+import CountdownTimer from "../components/CountdownTimer.vue"
+import ListningComprehension from "../components/listningComprehension.vue"
 
-    export default {
-        components: {
-            CountdownTimer,
-            listningComprehension
-        }
-    }
+const showTimer = ref(false)
 </script>
 
 <template>
-    <CountdownTimer />
+     <div class="timer-btn-container">
+        <button class="btn timer-btn" @click="showTimer = !showTimer">
+            Timer⏰
+        </button>
+        </div>
+
+        <div v-if="showTimer" class="card">
+            <CountdownTimer />
+    </div>
+
     <listningComprehension />
     <div id="levels-container">
         <router-link to="/levels" class="back-link"
@@ -21,16 +26,30 @@
 </template>
 
 <style scoped>
+    .timer-btn-container{
+        display: flex;
+        justify-content: center; 
+        align-items: center;
+    }
+    .timer-btn {
+        width: 100%;
+        max-width: 200px;
+        background-color: #0b0b62;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+        }
+    .timer-btn p {
+        margin: 0;
+    }
     .back-link {
         margin-top: 24px;
         color: #0b0b62;
         font-weight: 600;
         text-decoration: none;
         font-size: 0.95rem;
-    }
-
-    .back-link:hover {
-        text-decoration: underline;
     }
 
     .back-link:hover {
--- src/views/wordsAndPhrasesView.vue
+++ src/views/wordsAndPhrasesView.vue
@@ -1,36 +1,57 @@
-<script>
-    import CountdownTimer from "../components/CountdownTimer.vue"
-    import wordsAndPhrases from "../components/wordsAndPhrases.vue"
+<script setup>
+import { ref } from "vue"
+import CountdownTimer from "../components/CountdownTimer.vue"
+import WordsAndPhrases from "../components/wordsAndPhrases.vue"
 
-    export default {
-        components: {
-            CountdownTimer,
-            wordsAndPhrases
-        }
-    }
+const showTimer = ref(false)
 </script>
 
 <template>
+  <div class="timer-btn-container">
+  <button class="btn timer-btn" @click="showTimer = !showTimer">
+    Timer⏰
+  </button>
+  </div>
+
+  <div v-if="showTimer" class="card">
     <CountdownTimer />
-    <wordsAndPhrases />
-    <div id="levels-container">
-        <router-link to="/levels" class="back-link"
-            >← Back to levels</router-link
-        >
-    </div>
+  </div>
+
+  <WordsAndPhrases />
+
+  <div id="levels-container">
+    <router-link to="/levels" class="back-link">
+      ← Back to levels
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
+
+    .timer-btn-container{
+        display: flex;
+        justify-content: center; 
+        align-items: center;
+    }
+    .timer-btn {
+        width: 100%;
+        max-width: 200px;
+        background-color: #0b0b62;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.5rem;
+        }
+    .timer-btn p {
+        margin: 0;
+    }
     .back-link {
         margin-top: 24px;
         color: #0b0b62;
         font-weight: 600;
         text-decoration: none;
         font-size: 0.95rem;
-    }
-
-    .back-link:hover {
-        text-decoration: underline;
     }
 
     .back-link:hover {
