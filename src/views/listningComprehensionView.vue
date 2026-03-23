03c67228a1ce70531f737b09ca894aa19af746c9
Erika Matsumoto <matmoterika816@gmail.com>
2026-03-23 14:20:53 +0100

    Added wordsAndPhrases och listningComprehension.vue

---
+++ src/views/listningComprehensionView.vue
@@ -0,0 +1,39 @@
+<script>
+    import CountdownTimer from "../components/CountdownTimer.vue"
+    import listningComprehension from "../components/listningComprehension.vue"
+
+    export default {
+        components: {
+            CountdownTimer,
+            listningComprehension
+        }
+    }
+</script>
+
+<template>
+    <CountdownTimer />
+    <listningComprehension />
+    <div id="levels-container">
+        <router-link to="/levels" class="back-link"
+            >← Back to levels</router-link
+        >
+    </div>
+</template>
+
+<style scoped>
+    .back-link {
+        margin-top: 24px;
+        color: #0b0b62;
+        font-weight: 600;
+        text-decoration: none;
+        font-size: 0.95rem;
+    }
+
+    .back-link:hover {
+        text-decoration: underline;
+    }
+
+    .back-link:hover {
+        text-decoration: underline;
+    }
+</style>
---
+++ src/views/wordsAndPhrasesView.vue
@@ -0,0 +1,39 @@
+<script>
+    import CountdownTimer from "../components/CountdownTimer.vue"
+    import wordsAndPhrases from "../components/wordsAndPhrases.vue"
+
+    export default {
+        components: {
+            CountdownTimer,
+            wordsAndPhrases
+        }
+    }
+</script>
+
+<template>
+    <CountdownTimer />
+    <wordsAndPhrases />
+    <div id="levels-container">
+        <router-link to="/levels" class="back-link"
+            >← Back to levels</router-link
+        >
+    </div>
+</template>
+
+<style scoped>
+    .back-link {
+        margin-top: 24px;
+        color: #0b0b62;
+        font-weight: 600;
+        text-decoration: none;
+        font-size: 0.95rem;
+    }
+
+    .back-link:hover {
+        text-decoration: underline;
+    }
+
+    .back-link:hover {
+        text-decoration: underline;
+    }
+</style>
